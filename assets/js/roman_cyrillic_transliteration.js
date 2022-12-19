5e0c6ec4553e6626b339aab81b8f70b877092380
socotra567118 <89115950+socotra567118@users.noreply.github.com>
2022-12-19 17:22:29 +0900

    2022/12/19 UTC +09:00 17:22

--- assets/js/roman_cyrillic_transliteration.js
+++ assets/js/roman_cyrillic_transliteration.js
@@ -1,22 +1,37 @@
-const upper_cyrillic_to_upper_roman_dictionary = {"А": "A", "Б": "B", "В": "V", "Г": "G", "Д": "D", "Е": "E", "Ё": "Yo", "Ж": "Zh", "З": "Z", "И": "I", "Й": "J", "К": "K", "Л": "L", "М": "M", "Н": "N", "О": "O", "П": "п", "Р": "R", "С": "C", "Т": "T", "У": "U", "Ф": "F", "Х": "Kh", "Ц": "Cz", "Ч": "Ch", "Ш": "Sh", "Щ": "Shh", "Ъ": "", "Ы": "Y", "Ь": "", "Э": "E", "Ю": "Yu", "Я": "Ya", "Ѓ": "G", "Ґ": "G", "Є": "Ye", "S": "Z", "Ј": "J", "Ї": "Yi", "Ќ": "K", "Љ": "L", "Њ": "N", "Ў": "U", "Џ": "Dh"}
+const upper_cyrillic_to_upper_roman_dictionary = {"А": "A", "Б": "B", "В": "V", "Г": "G", "Д": "D", "Е": "E", "Ё": "Yo", "Ж": "Zh", "З": "Z", "И": "I", "Й": "J", "К": "K", "Л": "L", "М": "M", "Н": "N", "О": "O", "П": "P", "Р": "R", "С": "C", "Т": "T", "У": "U", "Ф": "F", "Х": "Kh", "Ц": "Cz", "Ч": "Ch", "Ш": "Sh", "Щ": "Shh", "Ъ": "", "Ы": "Y", "Ь": "", "Э": "E", "Ю": "Yu", "Я": "Ya", "Ѓ": "G", "Ґ": "G", "Є": "Ye", "S": "Z", "Ј": "J", "Ї": "Yi", "Ќ": "K", "Љ": "L", "Њ": "N", "Ў": "U", "Џ": "Dh"}
 
-const lower_cyrillic_to_upper_roman_dictionary = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'j', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'п', 'р': 'r', 'с': 'c', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'cz', 'ч': 'ch', 'ш': 'sh', 'щ': 'shh', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya', 'ѓ': 'g', 'ґ': 'g', 'є': 'ye', 's': 'z', 'ј': 'j', 'ї': 'yi', 'ќ': 'k', 'љ': 'l', 'њ': 'n', 'ў': 'u', 'џ': 'dh'}
-
-const if_the_language_is_bulgarian_upper_to_upper_roman = {"Ъ": "A"}
-const if_the_language_is_bulgarian_lower_to_lower_roman = {"ъ": "a"}
+const lower_cyrillic_to_upper_roman_dictionary = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'j', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 'c', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'cz', 'ч': 'ch', 'ш': 'sh', 'щ': 'shh', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya', 'ѓ': 'g', 'ґ': 'g', 'є': 'ye', 's': 'z', 'ј': 'j', 'ї': 'yi', 'ќ': 'k', 'љ': 'l', 'њ': 'n', 'ў': 'u', 'џ': 'dh'}
 
 var change = document.getElementById('change');             // 바꾸기 버튼 변수
 var clear = document.getElementById('clear');               // 지우기 버튼 변수
 var copy = document.getElementById('copy');                 // 복사하기 버튼 변수
+var isFromRoman = document.getElementById("isFromRoman")    // 로마자→키릴문자 체크박스 변수
+var isToRoman = document.getElementById("isToRoman")        // 키릴문자→로마자 체크박스 변수
 
 // 로마자-키릴문자 변환
 change.addEventListener('click', function(){
-    const textBox = document.getElementById("from").value;  // 사용자가 입력한 값 가져오기
-    const splitedTextBox = textBox.split("");               // 사용자가 입력한 값 알파벳 단위로 분해하기
-    var changedValue = "";                                  // 변경된 값을 담을 문자열
+    const textBox = document.getElementById("from").value;          // 사용자가 입력한 값 가져오기
+    const splitedTextBox = textBox.split("");                       // 사용자가 입력한 값 알파벳 단위로 분해하기
+    const isFrom = isFromRoman.checked;                             // 로마자→키릴문자 체크박스 체크 여부
+    var changedValue = "";                                          // 변경된 값을 담을 문자열
+
+    if (isFrom) {
+        // 로마자→키릴문자에 체크되어 있으면
+        changedValue = changeRomanToCyrillic(splitedTextBox);
+    } else {
+        // 키릴문자→로마자에 체크되어 있으면
+        changedValue = changeCyrillicToRoman(splitedTextBox);
+    }
+
+    document.getElementById("to").value = changedValue;
+})
+
+// 키릴문자→로마자 변환 함수
+function changeCyrillicToRoman(splitedValue) {
+    var changedValue = "";                                          // 변경된 값을 담을 문자열
 
     // 루프 돌면서 문자열에 변경된 값 담기
-    for (const alphabet of splitedTextBox) {
+    for (const alphabet of splitedValue) {
         const alphabetUnicode = alphabet.charCodeAt(0);     // 유니코드로 변환
 
         // 키릴문자 범위 안에 있는지 확인
@@ -35,8 +50,35 @@ change.addEventListener('click', function(){
         }
     }
 
-    document.getElementById("to").value = changedValue;
-})
+    return changedValue
+}
+
+// 로마자→키릴문자 변환함수
+function changeRomanToCyrillic(splitedValue) {
+    var changedValue = "";                                          // 변경된 값을 담을 문자열
+
+    // 루프 돌면서 문자열에 변경된 값 담기
+    for (const alphabet of splitedValue) {
+        const alphabetUnicode = alphabet.charCodeAt(0);     // 유니코드로 변환
+
+        // 키릴문자 범위 안에 있는지 확인
+        if ((alphabetUnicode >= 1024) && (alphabetUnicode <= 1279)) {
+            // 키릴문자면
+            if (alphabet === alphabet.toUpperCase()) {
+                // 대문자라면 대문자로
+                changedValue += upper_cyrillic_to_upper_roman_dictionary[alphabet];
+            } else if (alphabet === alphabet.toLowerCase()) {
+                // 소문자라면 소문자로
+                changedValue += lower_cyrillic_to_upper_roman_dictionary[alphabet];
+            } 
+        } else {
+            // 키릴문자가 아니면 똑같이 출력
+            changedValue += alphabet;
+        }
+    }
+
+    return changedValue
+}
 
 // 입력 내용 지우기
 clear.addEventListener('click', function() {
@@ -54,4 +96,21 @@ copy.addEventListener('click', function() {
         .catch(err => {
         console.log('Something went wrong', err);
     })
+})
+
+// 로마자→키릴문자, 키릴문자→로마자 체크박스 하나만 선택되게 하기
+isFromRoman.addEventListener('click', function() {
+    if (isFromRoman.checked) {
+        isToRoman.checked = false;
+    } else {
+        isToRoman.checked = true;
+    }
+})
+
+isToRoman.addEventListener('click', function() {
+    if (isToRoman.checked) {
+        isFromRoman.checked = false;
+    } else {
+        isFromRoman.checked = true;
+    }
 })
